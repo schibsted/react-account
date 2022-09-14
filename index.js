bf3a216a63b785856d07252ba5f9154844636590
Sverre Norberg-Schulz Hagen <sverre.norberg-schulz.hagen@schibsted.no>
2022-09-14 14:34:07 +0200

    fix: use relative path for exports

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-export { default, AccountContext } from "lib/AccountProvider.js";
-export { default as useAccount } from "lib/useAccount.js";
+export { default, AccountContext } from "./lib/AccountProvider.js";
+export { default as useAccount } from "./lib/useAccount.js";
