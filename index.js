d14f758e2bff8f291ff036a7d2d770c2379cf0e3
Sverre Norberg-Schulz Hagen <sverre.norberg-schulz.hagen@schibsted.no>
2022-09-16 10:02:31 +0200

    fix: add missing exports from index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 export { AccountProvider, AccountContext } from "./AccountProvider.js";
 export { Identity } from "./Identity.js";
+export { Monetization } from "./Monetization.js";
+export { Payment } from "./Payment.js";
 export { useAccount } from "./useAccount.js";
