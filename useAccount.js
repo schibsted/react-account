fd0778140cfbca1ecdeb8f27a7973ad98940b838
Sverre Norberg-Schulz Hagen <sverre.norberg-schulz.hagen@schibsted.no>
2022-10-19 10:36:41 +0200

    feat: take state as parameter to login function

--- useAccount.js
+++ useAccount.js
@@ -6,8 +6,11 @@ export const useAccount = () => {
   const { account, user, isLoggedIn } = useContext(AccountContext);
 
   const login = useCallback(
-    function login(redirectUrl = window.location.href) {
-      account.login({ redirectUrl, state: Math.random().toString(16) });
+    function login({
+      redirectUrl = window.location.href,
+      state = Math.random().toString(16),
+    }) {
+      account.login({ redirectUrl, state });
     },
     [account]
   );
